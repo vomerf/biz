9b7710e7804bb210f50a795eae9f0a53439ced2a
Nikita <vomerff@gmail.com>
2025-08-20 15:26:39 +0700

    update readme

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -16,10 +16,11 @@ router = APIRouter()
 
 @router.post("/register", response_model=UserRead)
 async def register(user: UserCreate, db: AsyncSession = Depends(get_session)):
+    """Регистрация пользователя"""
     existing_user = await get_user_by_email(db, user.email)
     if existing_user:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered"
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Пользователь с таким email уже существует."
         )
     new_user = await create_user(db, user.email, user.password)
     return new_user
@@ -27,9 +28,13 @@ async def register(user: UserCreate, db: AsyncSession = Depends(get_session)):
 
 @router.post("/login", response_model=TokenResponse)
 async def login(user: UserCreate, db: AsyncSession = Depends(get_session)):
+    """Логинимся пользователем
+
+    Получаем access_token и refresh_token токен.
+    """
     db_user = await get_user_by_email(db, user.email)
     if not db_user or not verify_password(user.password, db_user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Невалижные учетные данные")
     access_token = create_access_token(db_user.email)
     refresh_token = create_refresh_token(db_user.email)
     return {"access_token": access_token, "refresh_token": refresh_token}
@@ -37,6 +42,7 @@ async def login(user: UserCreate, db: AsyncSession = Depends(get_session)):
 
 @router.get("/me", response_model=UserRead)
 async def read_me(current_user: User = Depends(get_current_user)):
+    """Проверка токена и получаение своих данных"""
     return current_user
 
 
@@ -48,6 +54,7 @@ async def is_superuser(current_user: User = Depends(current_superuser)):
 
 @router.post("/refresh", response_model=TokenResponse)
 async def refresh_token(req: RefreshRequest):
+    """С помощью refersh_token получаем новый access_token"""
     try:
         payload = jwt.decode(
             req.refresh_token,
@@ -63,8 +70,6 @@ async def refresh_token(req: RefreshRequest):
     if not email:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Невалидный refresh токен")
 
-    # создаём только новый access-токен
-    # после того как refresh истечет, нужно будет заново логиниться
     new_access = create_access_token(email)
 
     return TokenResponse(access_token=new_access, refresh_token=req.refresh_token)
--- app/api/routers.py
+++ app/api/routers.py
@@ -4,4 +4,4 @@ from .endpoints import auth_router
 
 main_router = APIRouter()
 
-main_router.include_router(auth_router)
+main_router.include_router(auth_router, tags=["Auth"])
